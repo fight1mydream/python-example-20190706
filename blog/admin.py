95ab8378192be9525b46e1a093ba8d98ae5c171d
fight1mydream <52594262+fight1mydream@users.noreply.github.com>
2019-07-06 17:26:06 +0900

    blog.Post모델을 admin에 등록

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post
+
+#admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display=['title','created_at']
+    list_filter=['updated_at']
+    search_fields=['title']
 
 # Register your models here.
+
